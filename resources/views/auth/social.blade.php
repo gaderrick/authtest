2b02dcbc4ede0b4ce2b9f2dc7ba0f4cfb6bef8cb
Gerald Derrick <jerry.derrick@gmail.com>
2018-04-16 13:18:16 -0500

    Added modifications to social components

--- resources/views/auth/social.blade.php
+++ resources/views/auth/social.blade.php
@@ -18,22 +18,14 @@
                     <div class="panel-body">
                         <p class="lead text-center">Authenticate using your social network account from one of following
                             providers</p>
-                        <a href="{{ route('social.oauth', 'github') }}" class="btn btn-default btn-block">
-                            Login with Github
-                        </a>
-                        <a href="{{ route('social.oauth', 'facebook') }}" class="btn btn-primary btn-block">
-                            Login with Facebook
-                        </a>
-                        <a href="{{ route('social.oauth', 'twitter') }}" class="btn btn-info btn-block">
-                            Login with Twitter
-                        </a>
-                        <a href="{{ route('social.oauth', 'google') }}" class="btn btn-danger btn-block">
-                            Login with Google
-                        </a>
-                        <hr>
-                        <a href="{{ route('login') }}" class="btn btn-default btn-block">
-                            Login with Email
-                        </a>
+                        <a href="{{ route('social.oauth', 'twitter') }}" class="btn btn-info btn-block"><img width="36"
+                                                                                                             src="images/icon-twitter-logo.svg">Twitter</a>
+                        <a href="{{ route('social.oauth', 'github') }}" class="btn btn-default btn-block"><img
+                                width="36" src="images/icon-linkedin-logo.svg">Github</a>
+                        <a href="{{ route('social.oauth', 'facebook') }}" class="btn btn-primary btn-block"><img
+                                width="36" src="images/icon-facebook-logo.svg">Facebook</a>
+                        <a href="{{ route('social.oauth', 'google') }}" class="btn btn-danger btn-block"><img width="36"
+                                                                                                              src="images/icon-google-plus-logo.svg">Google</a>
                     </div>
                 </div>
             </div>
